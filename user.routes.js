6d77a0609f751808f9f1a99cbdc59aa9b35938ae
mohammad <you@example.com>
2017-12-03 21:04:16 -0500

    added ability to follow users

--- user.routes.js
+++ user.routes.js
@@ -1,5 +1,5 @@
  /* 
-	localhost:3000/post/<Route>
+	localhost:3000/user/<Route>
 */
 const Router = require('express').Router(),
 	User 	 = require('./model/user.model'),
@@ -14,15 +14,65 @@ Router.get('/profile', (req,res) => {
 })
 
 Router.get('/profile/:username',(req,res) =>{
+	queryUserIsSessionUser = req.session.user.username === req.params.username;
+
 	User.findOne({ username: req.params.username }).then((user) => {
 		Post.find({ original_poster: user._id }).then((posts) => {
-			res.render('profile',{posts,user});
+			res.render('profile',{posts,user, queryUserIsSessionUser,});
 		})
 	})
 })
 
 
+Router.get('/settings',(req,res) => {
+	if(req.session.user) {
+		User.findOne({ username: req.session.user.username }).then((user) => {
+			
+
+
+			Post.find({ original_poster: user._id }).then((posts) => {
+
+				console.log(user);
+
+				console.log(posts);
+				res.render('settings',{posts,user,});
+			})
+		}).catch((err) => {
+			console.log(err);
+			res.redirect('/');
+		 })
+	}else{
+		res.redirect('/');
+	}
+
+})
+
+Router.get('/following/new/:username',(req,res) => {
+	let queryUserExists;
+	User.findOne({username: req.params.username}).then((user) => {
+		queryUserExists = user;
+		user.followers.push(req.session.user.username);
+		user.save();
+	}).then(() => {
+		if(queryUserExists) {
+			User.findOne({ username: req.session.user.username }).then((user) => {
+				user.followings.push(req.params.username);
+				user.save();
+				res.redirect('/dashboard');
+			})
+		}else{
+			res.redirect('/');
+		}
+	}).catch((err) => {
+		console.log(err);
+		res.redirect('/');
+	})
+
+
+})
+
 
 module.exports = Router;
 
 
+// /user/following/remove/tim1
