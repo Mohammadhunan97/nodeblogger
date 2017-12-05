4e97211b650430ca76bdb0e232670229aedd41ec
mohammad <you@example.com>
2017-12-05 17:24:25 -0500

    added nodemodules/key.js

---
+++ key.js
@@ -0,0 +1,13 @@
+module.exports = {
+	google: {
+		client_ID: 'your client id',
+		client_secret: 'your client secret'
+	},
+	facebook: {
+		client_ID: 'your client id',
+		client_secret: 'your client secret'
+	},
+	session: {
+		secret: 'tsuru'
+	}
+}
