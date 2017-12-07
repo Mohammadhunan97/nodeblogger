fc9a2f8d993f163ad72860f5b6a3045973783114
mohammad <you@example.com>
2017-12-07 13:41:03 -0500

    added key.js for heroku

---
+++ key.js
@@ -0,0 +1,17 @@
+module.exports = {
+	facebook: {
+		client_ID: '363548087429452',
+		client_secret: '36f9fbbb76d448fd20ea9409736c425b'
+	},
+	session: {
+		secret: 'tsuru'
+	},
+	db: {
+		local: 'computing6',
+		remote: 'mongodb://heroku_kpfmrq31:3g7f00e92n87mhtnjbt4qt3nr1@ds131826.mlab.com:31826/heroku_kpfmrq31'
+	},
+	sendGrid: {
+		username: 'app82398077@heroku.com',
+		password: 'n5muap6y6844'
+	}
+}
